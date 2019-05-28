6ac446fe1f0de49af05903153b3e5e441211675f
lj <lslj0516@163.com>
2019-05-28 19:30:20 +0800

    add: 0106

--- solutions/0105.go
+++ solutions/0105.go
@@ -41,7 +41,7 @@ func tree(preorder []int, ps int, pe int, inorder []int, is int, ie int) *ds.Tre
 
 func buildTree105(preorder []int, inorder []int) *ds.TreeNode {
 	plen := len(preorder)
-	ilen := len(preorder)
+	ilen := len(inorder)
 	if preorder == nil || inorder == nil || plen != ilen || plen == 0 {
 		return nil
 	}
@@ -72,10 +72,12 @@ Return the following binary tree:
 		Method: reflect.ValueOf(buildTree105),
 		Tests:  make([]TestCase, 0),
 	}
+	/* no test case
 	a := TestCase{}
 	a.Input = []interface{}{[]int{2, 7, 11, 15}, 9}
 	a.Output = []interface{}{[]int{0, 1}}
 	sol.Tests = append(sol.Tests, a)
+	*/
 
 	SolutionMap["0105"] = sol
 }
--- solutions/0106.go
+++ solutions/0106.go
@@ -6,8 +6,47 @@ import (
 	"pokman/bulbasaur/leetcode/ds"
 )
 
+func tree(postorder []int, ps int, pe int, inorder []int, is int, ie int) *ds.TreeNode {
+	if ps > pe || is > ie {
+		return nil
+	}
+	if ps == pe || is == ie {
+		return &ds.TreeNode{
+			Val:   postorder[ps],
+			Left:  nil,
+			Right: nil,
+		}
+	}
+	f := postorder[pe]
+	mid_idx := is
+	for i := ie; i >= is; i-- {
+		if f == inorder[i] {
+			mid_idx = i
+			break
+		}
+	}
+	left_len := mid_idx - is
+	right_len := ie - mid_idx
+	left := tree(postorder, ps, ps+left_len-1, inorder, is, mid_idx-1)
+	right := tree(postorder, ps+left_len, pe-1, inorder, mid_idx+1, mid_idx+right_len)
+
+	fnode := &ds.TreeNode{
+		Val:   f,
+		Left:  left,
+		Right: right,
+	}
+
+	return fnode
+}
+
 func buildTree106(inorder []int, postorder []int) *ds.TreeNode {
-	return nil
+	plen := len(postorder)
+	ilen := len(inorder)
+	if postorder == nil || inorder == nil || plen != ilen || plen == 0 {
+		return nil
+	}
+
+	return tree(postorder, 0, plen-1, inorder, 0, ilen-1)
 }
 
 func init() {
@@ -19,10 +58,12 @@ func init() {
 		Method: reflect.ValueOf(buildTree106),
 		Tests:  make([]TestCase, 0),
 	}
+	/* no test case
 	a := TestCase{}
 	a.Input = []interface{}{[]int{2, 7, 11, 15}, 9}
 	a.Output = []interface{}{[]int{0, 1}}
 	sol.Tests = append(sol.Tests, a)
+	*/
 
 	SolutionMap["0106"] = sol
 }
