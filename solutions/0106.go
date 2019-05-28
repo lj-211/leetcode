c9da18e0bb54f536c6b170d7c1299ad48a6e6644
lj <lslj0516@163.com>
2019-05-28 19:37:04 +0800

    add: 0106 desc

--- solutions/0106.go
+++ solutions/0106.go
@@ -51,6 +51,21 @@ func buildTree106(inorder []int, postorder []int) *ds.TreeNode {
 
 func init() {
 	desc := `
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
 	`
 	sol := Solution{
 		Title:  "Construct Binary Tree from Inorder and Postorder Traversal",
