34338178f23a1ea76ebde5fc873320ace35c8760
lj <lslj0516@163.com>
2019-06-24 09:02:06 +0800

    add: 0070 & test case

--- solutions/0070.go
+++ solutions/0070.go
@@ -5,7 +5,29 @@ import (
 )
 
 func climbStairs(n int) int {
-	return 0
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	/*
+		dp := make([]int, n)
+		dp[0] = 1
+		dp[1] = 2
+		for i := 2; i < n; i++ {
+			dp[i] = dp[i-1] + dp[i-2]
+		}
+	*/
+	one := 2
+	two := 1
+	cur := 0
+	for i := 2; i < n; i++ {
+		cur = one + two
+		two = one
+		one = cur
+	}
+	return cur
 }
 
 func init() {
