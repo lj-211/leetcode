796ce5b199cb71c1a43909d84815fb1216ffbcfe
lj <lslj0516@163.com>
2019-07-12 15:30:15 +0800

    add: greedy

--- solutions/0045.go
+++ solutions/0045.go
@@ -5,6 +5,20 @@ import (
 	"reflect"
 )
 
+/* 贪心
+public int jump(int[] A) {
+	int jumps = 0, curEnd = 0, curFarthest = 0;
+	for (int i = 0; i < A.length - 1; i++) {
+		curFarthest = Math.max(curFarthest, i + A[i]);
+		if (i == curEnd) {
+			jumps++;
+			curEnd = curFarthest;
+		}
+	}
+	return jumps;
+}
+*/
+
 func jump(nums []int) int {
 	size := len(nums)
 	if size <= 1 {
