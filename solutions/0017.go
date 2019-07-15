86f730f3732db27e93895714214485ff62570f53
lj <lslj0516@163.com>
2019-07-15 09:32:39 +0800

    add: 0017 & test case

--- solutions/0017.go
+++ solutions/0017.go
@@ -4,6 +4,40 @@ import (
 	"reflect"
 )
 
+var DigitMap map[byte][]string = map[byte][]string{
+	'2': []string{"a", "b", "c"},
+	'3': []string{"d", "e", "f"},
+	'4': []string{"g", "h", "i"},
+	'5': []string{"j", "k", "l"},
+	'6': []string{"m", "n", "o"},
+	'7': []string{"p", "q", "r", "s"},
+	'8': []string{"t", "u", "v"},
+	'9': []string{"w", "x", "y", "z"},
+}
+
+func recurComb(digits string) []string {
+	size := len(digits)
+	if size == 0 {
+		return []string{}
+	}
+
+	d := digits[0]
+	dslist := DigitMap[d]
+	ret := make([]string, 0)
+	comList := recurComb(digits[1:])
+	for _, v := range dslist {
+		if len(comList) > 0 {
+			for _, v2 := range comList {
+				ret = append(ret, v+v2)
+			}
+		} else {
+			ret = append(ret, v)
+		}
+	}
+
+	return ret
+}
+
 func letterCombinations(digits string) []string {
 	return []string{}
 }
@@ -23,7 +57,7 @@ Although the above answer is in lexicographical order, your answer could be in a
 	sol := Solution{
 		Title:  "Letter Combinations of a Phone Number",
 		Desc:   desc,
-		Method: reflect.ValueOf(twoSum),
+		Method: reflect.ValueOf(recurComb),
 		Tests:  make([]TestCase, 0),
 	}
 	a := TestCase{}
