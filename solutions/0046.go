be59923a1d36695955e7fd5f40df87d362740459
lj <lslj0516@163.com>
2019-07-16 09:13:51 +0800

    add: 0046 & test case

--- solutions/0046.go
+++ solutions/0046.go
@@ -4,8 +4,35 @@ import (
 	"reflect"
 )
 
+func bt(nums []int, mask map[int]bool, one []int, result *[][]int) {
+	if len(nums) == len(one) {
+		dst := make([]int, len(one))
+		copy(dst, one)
+		*result = append(*result, dst)
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		v := nums[i]
+		if _, ok := mask[v]; ok {
+			continue
+		}
+
+		one = append(one, v)
+		mask[v] = true
+		bt(nums, mask, one, result)
+		one = one[0 : len(one)-1]
+		delete(mask, v)
+	}
+}
+
 func permute(nums []int) [][]int {
-	return [][]int{[]int{}}
+	mask := make(map[int]bool)
+	one := make([]int, 0)
+	result := make([][]int, 0)
+
+	bt(nums, mask, one, &result)
+
+	return result
 }
 
 func init() {
@@ -31,8 +58,10 @@ Output:
 		Method: reflect.ValueOf(permute),
 		Tests:  make([]TestCase, 0),
 	}
-	//a := TestCase{}
-	//sol.Tests = append(sol.Tests, a)
+	a := TestCase{}
+	a.Input = []interface{}{[]int{5, 4, 6, 2}}
+	a.Output = []interface{}{2}
+	sol.Tests = append(sol.Tests, a)
 
 	SolutionMap["0046"] = sol
 }
