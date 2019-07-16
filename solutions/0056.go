f2d54cbcd75569829f4e53f0f51df067cce63fb7
lj <lslj0516@163.com>
2019-07-16 16:28:44 +0800

    add: 0056 & test case

--- solutions/0056.go
+++ solutions/0056.go
@@ -1,11 +1,76 @@
 package solutions
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 )
 
+const (
+	Start = iota
+	End
+)
+
+type InfoTmp struct {
+	Val  int
+	Flag int
+}
+
+type Infos []InfoTmp
+
+func (s Infos) Len() int      { return len(s) }
+func (s Infos) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s Infos) Less(i, j int) bool {
+	if s[i].Val == s[j].Val {
+		return s[i].Flag < s[j].Flag
+	}
+	return s[i].Val < s[j].Val
+}
+
 func merge(intervals [][]int) [][]int {
-	return [][]int{}
+	size := len(intervals)
+	if size == 0 {
+		return [][]int{}
+	}
+	if size == 1 {
+		return intervals
+	}
+	var allInts Infos
+	for i := 0; i < len(intervals); i++ {
+		allInts = append(allInts, InfoTmp{
+			Val:  intervals[i][0],
+			Flag: Start,
+		})
+		allInts = append(allInts, InfoTmp{
+			Val:  intervals[i][1],
+			Flag: End,
+		})
+	}
+
+	sort.Sort(allInts)
+	fmt.Println(allInts)
+
+	ret := make([][]int, 0)
+
+	stack := 1
+	s := 0
+	for i := 1; i < len(allInts); i++ {
+		v := allInts[i]
+		if v.Flag == Start {
+			stack++
+		} else {
+			stack--
+			if stack == 0 {
+				one := make([]int, 2)
+				one[0] = allInts[s].Val
+				one[1] = allInts[i].Val
+				ret = append(ret, one)
+				s = i + 1
+			}
+		}
+	}
+
+	return ret
 }
 
 func init() {
@@ -31,9 +96,14 @@ NOTE: input types have been changed on April 15, 2019. Please reset to default c
 		Tests:  make([]TestCase, 0),
 	}
 	a := TestCase{}
-	a.Input = []interface{}{[]int{1, 4}, []int{4, 5}}
+	a.Input = []interface{}{[][]int{[]int{1, 4}, []int{4, 5}}}
 	a.Output = []interface{}{[]int{1, 5}}
 	sol.Tests = append(sol.Tests, a)
+	/*
+		a.Input = []interface{}{[][]int{[]int{1, 3}, []int{2, 6}, []int{8, 10}, []int{15, 18}}}
+		a.Output = []interface{}{[][]int{[]int{1, 6}, []int{8, 10}, []int{15, 18}}}
+		sol.Tests = append(sol.Tests, a)
+	*/
 
 	SolutionMap["0056"] = sol
 }
