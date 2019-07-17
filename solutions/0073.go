146b597a96a5d59c526ce0dfb7bf9e9e5c133033
lj <lslj0516@163.com>
2019-07-17 15:40:09 +0800

    add: 0073 & test case

--- solutions/0073.go
+++ solutions/0073.go
@@ -5,7 +5,38 @@ import (
 )
 
 func setZeroes(matrix [][]int) {
+	m := len(matrix)
+	n := 0
+	if m > 0 {
+		n = len(matrix[0])
+	}
+	if (m * n) == 0 {
+		return
+	}
+	col0 := 1
 
+	for i := 0; i < m; i++ {
+		if matrix[i][0] == 0 {
+			col0 = 0
+		}
+		for j := 1; j < n; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 1; j-- {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+		if col0 == 0 {
+			matrix[i][0] = 0
+		}
+	}
 }
 
 func init() {
