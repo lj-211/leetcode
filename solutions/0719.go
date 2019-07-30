1aae48a5bf113e8717f04fc6e44aa729a6f57f81
lj <lslj0516@163.com>
2019-07-30 09:20:09 +0800

    add: 0719 & test case

--- solutions/0719.go
+++ solutions/0719.go
@@ -1,11 +1,58 @@
 package solutions
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 )
 
 func smallestDistancePair(nums []int, k int) int {
-	return 0
+	size := len(nums)
+	if size <= 1 {
+		return 0
+	}
+
+	sort.Ints(nums)
+
+	min := 0
+	max := nums[size-1] - nums[0]
+
+	for min <= max {
+		mid := (min + max) / 2
+
+		cnt := 0   // 计算小于mid的个数
+		start := 0 // 当前最小值的游标，如果发生nums[i]-nums[start] > mid，则游标需要右移
+		maxDelta := 0
+
+		fmt.Println("------------- mid: ", mid)
+		for i := 0; i < size; i++ {
+			for start < size && nums[i]-nums[start] > mid {
+				start++
+			}
+			fmt.Println(nums[i], " - ", nums[start])
+			delta := nums[i] - nums[start]
+			fmt.Println("delta: ", delta)
+			if delta > maxDelta {
+				maxDelta = delta
+			}
+
+			cnt += (i - start)
+		}
+
+		fmt.Println("cnt-> ", cnt)
+		fmt.Println("start-> ", start)
+
+		if cnt == k {
+			return maxDelta
+		} else if cnt < k {
+			min = mid + 1
+		} else {
+			max = mid - 1
+		}
+		fmt.Println("min-max: ", min, max)
+	}
+
+	return min
 }
 
 func init() {
@@ -37,7 +84,18 @@ Note:
 		Tests:  make([]TestCase, 0),
 	}
 	a := TestCase{}
-	a.Input = []interface{}{[]int{1, 3, 1}, 0}
+	/*
+			a.Input = []interface{}{[]int{1, 3, 1}, 1}
+			a.Output = []interface{}{0}
+			sol.Tests = append(sol.Tests, a)
+		a.Input = []interface{}{[]int{1, 1, 1}, 2}
+		a.Output = []interface{}{0}
+		sol.Tests = append(sol.Tests, a)
+		a.Input = []interface{}{[]int{62, 100, 4}, 2}
+		a.Output = []interface{}{58}
+		sol.Tests = append(sol.Tests, a)
+	*/
+	a.Input = []interface{}{[]int{1, 1, 1}, 2}
 	a.Output = []interface{}{0}
 	sol.Tests = append(sol.Tests, a)
 
