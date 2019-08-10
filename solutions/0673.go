cb0dc6cf99f97664a72cd2fd1233694708e56508
lj <lslj0516@163.com>
2019-08-10 09:57:33 +0800

    add: 0673 dp解法 & test case

--- solutions/0673.go
+++ solutions/0673.go
@@ -1,11 +1,62 @@
 package solutions
 
 import (
+	"fmt"
 	"reflect"
 )
 
 func findNumberOfLIS(nums []int) int {
-	return 0
+	size := len(nums)
+	if size == 0 {
+		return 0
+	}
+
+	dp := make([]int, size)
+	dp[0] = 1
+	dpCnt := make([]int, size)
+	dpCnt[0] = 1
+
+	for i := 0; i < size; i++ {
+		max := 1
+		cnt := 1
+		for j := 0; j < i; j++ {
+			if nums[i] <= nums[j] {
+				continue
+			}
+			newDp := dp[j] + 1
+			if newDp > max {
+				max = newDp
+				cnt = dpCnt[j]
+			} else if newDp == max {
+				cnt += dpCnt[j]
+			}
+		}
+		if false && max == 1 {
+			cnt++
+		}
+		dp[i] = max
+		dpCnt[i] = cnt
+	}
+
+	fmt.Println(dp)
+	fmt.Println(dpCnt)
+
+	max := 0
+	maxCnt := 1
+	for i := 0; i < size; i++ {
+		v := dp[i]
+		if v > max {
+			max = v
+			maxCnt = dpCnt[i]
+			fmt.Println("设置: ", max, maxCnt, i, "-", dpCnt)
+		} else if v == max {
+			maxCnt += dpCnt[i]
+		}
+	}
+	fmt.Println("max: ", max)
+	fmt.Println("max: ", maxCnt)
+
+	return maxCnt
 }
 
 func init() {
@@ -27,12 +78,21 @@ func init() {
 	sol := Solution{
 		Title:  "最长递增子序列的个数",
 		Desc:   desc,
-		Method: reflect.ValueOf(twoSum),
+		Method: reflect.ValueOf(findNumberOfLIS),
 		Tests:  make([]TestCase, 0),
 	}
 	a := TestCase{}
+
 	a.Input = []interface{}{[]int{1, 3, 5, 4, 7}}
 	a.Output = []interface{}{2}
+	sol.Tests = append(sol.Tests, a)
+
+	a.Input = []interface{}{[]int{2, 2, 2, 2, 2}}
+	a.Output = []interface{}{5}
+	sol.Tests = append(sol.Tests, a)
+
+	a.Input = []interface{}{[]int{1, 2, 4, 3, 5, 4, 7, 2}}
+	a.Output = []interface{}{3}
 	sol.Tests = append(sol.Tests, a)
 
 	SolutionMap["0673"] = sol
