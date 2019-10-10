3d449f89679ee605408db923f3c9f8052276b972
lj <lslj0516@163.com>
2019-10-10 11:05:37 +0800

    add: adjust test rst

--- solutions/0039_test.go
+++ solutions/0039_test.go
@@ -12,8 +12,8 @@ func Test_0039_1(t *testing.T) {
 	in := []int{2, 3, 6, 7}
 	target := 7
 	eret := [][]int{
+		[]int{3, 2, 2},
 		[]int{7},
-		[]int{2, 2, 3},
 	}
 	ret := combinationSum(in, target)
 	log.Println(ret)
