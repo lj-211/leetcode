0ce87467df8546e76612e787c57722b6c2c1185e
lj <lslj0516@163.com>
2020-03-11 11:39:01 +0800

    70 rs with cache

--- solutions/0070.rs
+++ solutions/0070.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 struct Solution {}
 
 impl Solution {
@@ -11,7 +13,7 @@ impl Solution {
         let mut dp_pre = 1;
         let mut dp = 2;
 
-        for i in 3..=n {
+        for _i in 3..=n {
             let pre = dp;
             dp = dp + dp_pre;
             dp_pre = pre;
@@ -19,4 +21,45 @@ impl Solution {
 
         dp
     }
+
+    pub fn climb_stairs_v1(n: i32) -> i32 {
+        let mut cache = HashMap::new();
+
+        return Solution::internal_climb_staires(n, &mut cache);
+    }
+
+    fn internal_climb_staires(n: i32, cache: &mut HashMap<i32, i32>) -> i32 {
+        if n <= 2 {
+            cache.insert(n, n);
+            return n;
+        }
+
+        let one: i32;
+        let two: i32;
+
+        if let Some(v) = cache.get(&(n - 1)) {
+            one = *v;
+        } else {
+            one = Solution::internal_climb_staires(n - 1, cache);
+        }
+
+        if let Some(v) = cache.get(&(n - 2)) {
+            two = *v;
+        } else {
+            two = Solution::internal_climb_staires(n - 2, cache);
+        }
+
+        return one + two;
+    }
+}
+
+fn main() {
+    println!("problem 70:");
+    println!("-------------------------------");
+
+    let input = 4;
+    let output = Solution::climb_stairs_v1(input);
+    let output_2 = Solution::climb_stairs(input);
+    println!("v1: {}", output);
+    println!("{}", output_2);
 }
